093eee849d6acda996da65b6af2f8a63cc511821
Skylar <yutienh@student.unimelb.edu.au>
2022-02-07 14:58:10 +0800

    add MJ website JS files

---
+++ MJwebsite/js/fancy.js
@@ -0,0 +1,486 @@
+// JavaScript Document
+$(document).ready(function () {
+
+	// First slide
+	$('[data-fancybox="image1"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>GOT TO BE THERE</h3><p>January 24, 1972</p>\
+		      		  <p>1. Ain't No Sunshine</p>\
+		      		  <p>2. I Wanna Be Where You Are</p>\
+		      		  <p>3. Girl Don't Take Your Love from Me</p>\
+		      		  <p>4. In Our Small Way</p>\
+		      		  <p>5. Got to Be There</p>\
+		      		  <p>6. Rockin' Robin</p>\
+		      		  <p>7. Wings of My Love</p>\
+		      		  <p>8. Maria (You Were the Only One)</p>\
+		      		  <p>9. Love Is Here and Now You're Gone</p>\
+		      		  <p>10. You've Got a Friend</p>\
+		      		  "
+		    }
+		});
+		$('[data-fancybox="image2"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>BEN</h3><p>August 01, 1972</p>\
+		      		  <p>1. Ben</p>\
+		      		  <p>2. Greatest Show On Earth</p>\
+		      		  <p>3. People Make The World Go 'Round</p>\
+		      		  <p>4. We've Got A Good Thing Going</p>\
+		      		  <p>5. Everybody's Somebody's Fool</p>\
+		      		  <p>6. My Girl</p>\
+		      		  <p>7. What Goes Around Comes Around</p>\
+		      		  <p>8. In Our Small Way</p>\
+		      		  <p>9. Shoo-Be-Doo-Be-Doo-Da-Day</p>\
+		      		  <p>10. You Can Cry On My Shoulder</p>\
+		      		 ";
+		    }
+		});
+		$('[data-fancybox="image3"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>MUSIC AND ME</h3><p>October 23, 1973</p>\
+		      		 <p>1. Rockin' Robin</p>\
+		      		 <p>2. Johnny Raven</p>\
+		      		 <p>3. Shoo-Be-Doo-Be-Doo-Da-Day</p>\
+		      		 <p>4. Happy (Love Theme From 'Lady Sings The Blues')</p>\
+		      		 <p>5. Too Young</p>\
+		      		 <p>6. Up Again</p>\
+		      		 <p>7. With A Child's Heart</p>\
+		      		 <p>8. Ain't No Sunshine</p>\
+		      		 <p>9. Euphoria</p>\
+		      		 <p>10. Morning Glow</p>\
+		      		 <p>11. Music And Me</p>\
+		      		 <p>12. All The Things You Are, Are Mine</p>\
+		      		 <p>13. Cinderella Stay Awhile</p>\
+		      		 <p>14. We've Got Forever</p>\
+		      		";
+		    }
+		});
+
+
+		// Second slide
+		$('[data-fancybox="image4"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>FOREVER, MICHAEL</h3><p>January 01, 1975</p>\
+		      		  <p>1. We're Almost There</p>\
+		      		  <p>2. Take Me Back</p>\
+		      		  <p>3. One Day In Your Life</p>\
+		      		  <p>4. Cinderella Stay Awhile</p>\
+		      		  <p>5. We've Got Forever</p>\
+		      		  <p>6. Just A Little Bit Of You</p>\
+		      		  <p>7. You Are There</p>\
+		      		  <p>8. Dapper-Dan</p>\
+		      		  <p>9. Dear Michael</p>\
+		      		  <p>10. I'll Come Home To You</p>\
+		      		  "
+		    }
+		});
+		$('[data-fancybox="image5"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>OFFTHEWALL</h3><p>August 10, 1979</p>\
+		      		  <p>1. Don't Stop 'Til You Get Enough</p>\
+		      		  <p>2. Rock With You</p>\
+		      		  <p>3. Working Day And Night</p>\
+		      		  <p>4. Get On The Floor</p>\
+		      		  <p>5. Off The Wall</p>\
+		      		  <p>6. Girlfriend</p>\
+		      		  <p>7. She's Out Of My Life</p>\
+		      		  <p>8. I Can't Help It</p>\
+		      		  <p>9. It's The Falling In Love</p>\
+		      		  <p>10. Burn This Disco Out</p>\
+		      		";
+		    }
+		});
+		$('[data-fancybox="image6"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>THRILLER</h3><p>November 30, 1982</p>\
+		      		  <p>1. Wanna Be Startin' Somethin'</p>\
+		      		  <p>2. Baby Be Mine</p>\
+		      		  <p>3. Girl Is Mine (Vocal with Paul McCartney)</p>\
+		      		  <p>4. Thriller</p>\
+		      		  <p>5. Beat It</p>\
+		      		  <p>6. Billie Jean</p>\
+		      		  <p>7. Human Nature</p>\
+		      		  <p>8. P.Y.T.(Pretty Young Thing)</p>\
+		      		  <p>9. Lady in My Life</p>\
+		      		 ";
+		    }
+		});
+
+		
+		// Third slide
+		$('[data-fancybox="image7"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>BAD</h3><p>August 31, 1987</p>\
+		      		  <p>1. Bad</p>\
+		      		  <p>2. The Way You Make Me Feel</p>\
+		      		  <p>3. Speed Demon</p>\
+		      		  <p>4. Liberian Girl</p>\
+		      		  <p>5. Just Good Friends</p>\
+		      		  <p>6. Another Part Of Me</p>\
+		      		  <p>7. Man In The Mirror</p>\
+		      		  <p>8. I Just Can't Stop Loving You</p>\
+		      		  <p>9. Dirty Diana</p>\
+		      		  <p>10. Smooth Criminal</p>\
+		      		  <p>11. Leave Me Alone</p>\
+		      		  "
+		    }
+		});
+		$('[data-fancybox="image8"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>DANGEROUS</h3><p>November 26, 1991</p>\
+		      		  <p>1. Jam</p>\
+		      		  <p>2. Why You Wanna Trip On Me</p>\
+		      		  <p>3. In The Closet</p>\
+		      		  <p>4. She Drives Me Wild</p>\
+		      		  <p>5. Remember The Time</p>\
+		      		  <p>6. Can't Let Her Get Away</p>\
+		      		  <p>7. Heal The World</p>\
+		      		  <p>8. Black Or White</p>\
+		      		  <p>9. Who Is It</p>\
+		      		  <p>10. Give In To Me</p>\
+		      		  <p>11. Will You Be There</p>\
+		      		  <p>12. Keep The Faith</p>\
+		      		  <p>13. Gone Too Soon</p>\
+		      		  <p>14. Dangerous</p>\
+		      		 ";
+		    }
+		});
+		$('[data-fancybox="image9"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>REMEMBER THE TIME</h3><p>Jaunary 14, 1992</p>\
+		      		  <p>1. Remember The Time(SILKY SOUL 7'')</p>\
+		      		  <p>2. Remember The Time(NEW JACK RADIO MIX)</p>\
+		      		  <p>3. Remember The Time(12'' Main Mix)</p>\
+		      		  <p>4. Remember The Time(E-SMOOVE'S LATE NITE MIX)</p>\
+		      		  <p>5. Remember The Time(MAURICE'S UNDERGROUND)</p>\
+		      		  <p>6. Black Or White(THE CLIVILLES & COLE RADIO MIX)</p>\
+		      		  <p>7. Black Or White(HOUSE WITH GUITAR RADIO MIX)</p>\
+		      		  <p>8. Black Or White(THE CLIVILLES & COLE HOUSE/CLUB MIX)</p>\
+		      		  <p>9. Black Or White(THE UNDERGROUND CLUB MIX)</p>\
+		        	 ";
+		    }
+		});
+
+
+		// Fourth slide
+		$('[data-fancybox="image10"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>HISTORY</h3><p>June 20, 1995</p>\
+		      		  <p>1. Billie Jean</p>\
+		      		  <p>2. The Way You Make Me Feel</p>\
+		      		  <p>3. Black Or White</p>\
+		      		  <p>4. Rock With You</p>\
+		      		  <p>5. She's Out Of My Life</p>\
+		      		  <p>6. Bad</p>\
+		      		  <p>7. I Just Can't Stop Loving You(Vocal with Siedah Garrett)</p>\
+		      		  <p>8. Man In The Mirror</p>\
+		      		  <p>9. Thriller</p>\
+		      		  <p>10. Beat It</p>\
+		      		  <p>11. The Girl Is Mine(Vocal with Paul McCartney)</p>\
+		      		  <p>12. Remember The Time</p>\
+		      		  <p>13. Don't Stop 'Til You Get Enough</p>\
+		      		  <p>14. Wanna Be Startin' Somethin'</p>\
+		      		  <p>15. Heal The World</p>\
+		      		  <p>16. Scream</p>\
+		      		  <p>17. They Don't Care About Us</p>\
+		      		  <p>18. Stranger In Moscow</p>\
+		      		  <p>19. This Time Around</p>\
+		      		  <p>20. Earth Song</p>\
+		      		  <p>21. D.S.</p>\
+		      		  <p>22. Money</p>\
+		      		  <p>23. Come Together</p>\
+		      		  <p>24. You Are Not Alone</p>\
+		      		  <p>25. Childhood(Theme From 'Free Willy 2')</p>\
+		      		  <p>26. Tabloid Junkie</p>\
+		      		  <p>27. 2 Bad</p>\
+		      		  <p>28. History</p>\
+		      		  <p>29. Little Susie</p>\
+		      		  <p>30. Smile</p>\
+		      		  "
+		    }
+		});
+		$('[data-fancybox="image11"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>BLOOD ON THE DANCEFLOOR</h3><p>May 20, 1997</p>\
+		      		  <p>1. Blood On The Dance Floor</p>\
+		      		  <p>2. Morphine</p>\
+		      		  <p>3. Supafly Sister</p>\
+		      		  <p>4. Ghost</p>\
+		      		  <p>5. Is It Scary</p>\
+		      		  <p>6. Scream Louder(Flyte Tyme Remix)</p>\
+		      		  <p>7. Money(Fire Island Radio Edit)</p>\
+		      		  <p>8. 2 Bad(Refugee Camp Mix)</p>\
+		      		  <p>9. Stranger In Moscow(Tee's In-House Club Mix)</p>\
+		      		  <p>10. This Time Around(D.M. Radio Mix)</p>\
+		      		  <p>11. Earth Song(Hani's Extended Radio Experience)</p>\
+		      		  <p>12. You Are Not Alone(Classic Club Mix)</p>\
+		      		  <p>13. History(Tony Moran's HIStory Lesson)</p>\
+		      		 ";
+		    }
+		});
+		$('[data-fancybox="image12"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>INVINCIBLE</h3><p>October 30, 2001</p>\
+		      		  <p>1. Unbreakable</p>\
+		      		  <p>2. Heartbreaker</p>\
+		      		  <p>3. Invincible</p>\
+		      		  <p>4. Break Of Dawn</p>\
+		      		  <p>5. Heaven Can Wait</p>\
+		      		  <p>6. You Rock My World</p>\
+		      		  <p>7. Butterflies</p>\
+		      		  <p>8. Speechless</p>\
+		      		  <p>9. 2000 Watts</p>\
+		      		  <p>10. You Are My Life</p>\
+		      		  <p>11. Privacy</p>\
+		      		  <p>12. Don't Walk Away</p>\
+		      		  <p>13. Cry</p>\
+		      		  <p>14. The Lost Children</p>\
+		      		  <p>15. Whatever Happens</p>\
+		      		  <p>16. Threatened</p>\
+		      		 ";
+		    }
+		});
+
+
+		// Fifth slide
+		$('[data-fancybox="image13"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>NUMBER ONES</h3><p>November 18, 2003</p>\
+		      		  <p>1. Don't Stop Till You Get Enough (2003 Edit)</p>\
+		      		  <p>2. Rock With You</p>\
+		      		  <p>3. Billie Jean</p>\
+		      		  <p>4. Beat It</p>\
+		      		  <p>5. Thriller</p>\
+		      		  <p>6. I Just Can't Stop Loving You</p>\
+		      		  <p>7. Bad</p>\
+		      		  <p>8. Smooth Criminal</p>\
+		      		  <p>9. The Way You Make Me Feel</p>\
+		      		  <p>10. Man In The Mirror</p>\
+		      		  <p>11. Dirty Diana</p>\
+		      		  <p>12. Black Or White</p>\
+		      		  <p>13. You Are Not Alone</p>\
+		      		  <p>14. Earth Song</p>\
+		      		  <p>15. You Rock My World</p>\
+		      		  <p>16. Break Of Dawn</p>\
+		      		  <p>17. One More Chance</p>\
+		      		  <p>18. Ben</p>\
+		      		  "
+		    }
+		});
+		$('[data-fancybox="image14"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>ONE MORE CHANCE</h3><p>November 20, 2003</p>\
+		      		  <p>1. One More Chance</p>\
+		      		  <p>2. One More Chance(Paul Oakenfold Mix)</p>\
+		      		  <p>3. One More Chance(METRO Remix)</p>\
+		      		  <p>4. One More Chance(Ron G Club Remix)</p>\
+		      		  <p>5. One More Chance(Paul Oakenfold Urban Mix)</p>\
+		      		 ";
+		    }
+		});
+		$('[data-fancybox="image15"]').fancybox({
+		    baseClass: "fancybox-custom-layout",
+		    infobar: false,
+		    touch: {
+		      vertical: false
+		    },
+		    buttons: ["close", "thumbs", "share"],
+		    animationEffect: "fade",
+		    transitionEffect: "fade",
+		    preventCaptionOverlap: false,
+		    idleTime: false,
+		    gutter: 0,
+		    // Customize caption area
+		    caption: function(instance) {
+		      return "<h3>THIS IS IT</h3><p>October 27, 2009</p>\
+		      		  <p>1. Wanna Be Startin' Somethin'</p>\
+		      		  <p>2. Jam</p>\
+		      		  <p>3. They Don't Care About Us</p>\
+		      		  <p>4. Human Nature</p>\
+		      		  <p>5. Smooth Criminal</p>\
+		      		  <p>6. The Way You Make Me Feel</p>\
+		      		  <p>7. Shake Your Body (Down To The Ground)</p>\
+		      		  <p>8. I Just Can't Stop Loving You</p>\
+		      		  <p>9. Thriller</p>\
+		      		  <p>10. Beat It</p>\
+		      		  <p>11. Black Or White</p>\
+		      		  <p>12. Earth Song</p>\
+		      		  <p>13. Billie Jean</p>\
+		      		  <p>14. Man In The Mirror</p>\
+		      		  <p>15. This Is It</p>\
+		      		  <p>16. This Is It (Orchestra Version)</p>\
+		      		  <p>1. She's Out Of My Life (Demo)</p>\
+		      		  <p>2. Wanna Be Startin' Somethin' (Demo)</p>\
+		      		  <p>3. Beat It (Demo)</p>\
+		      		  <p>4. Planet Earth (Poem)</p>\
+		      		 ";
+		    }
+		});
+})
---
+++ MJwebsite/js/mj.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(document).ready(function () {
+    $("body").backgroundSlideshow({
+        transitionDuration: 3000,
+        fixed: true,
+        images: [
+            "img/home_mj1.jpg",
+            "img/home_mj2.jpg",
+            "img/home_mj4.jpg",
+            "img/home_mj5.jpg",
+        ]
+    })
+})
